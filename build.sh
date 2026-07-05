612cf98fbc4359c54d7982743d8eeb7a7f980476
xf00889 <hutchiejn@gmail.com>
2026-07-05 21:42:42 +0800

    fgys

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -x
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --fake-initial
 python manage.py createcachetable
 python manage.py shell -c "from django.core.cache import caches; [cache.clear() for cache in caches.all()]; print('Cleared Django caches')"
