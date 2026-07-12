8b11cab975e0fe6d50a91bd050f98cef14d12327
xf00889 <hutchiejn@gmail.com>
2026-07-12 13:26:26 +0800

    Fix duplicate bio migration and CI config

--- users/migrations/0002_student_bio_student_profile_picture_and_more.py
+++ users/migrations/0002_student_bio_student_profile_picture_and_more.py
@@ -2,11 +2,14 @@
 
 from django.db import migrations, models
 
+
 class Migration(migrations.Migration):
 
     dependencies = [
         ('users', '0001_initial'),
     ]
+
+    operations = [
         migrations.AddField(
             model_name='student',
             name='profile_picture',
